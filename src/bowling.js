b5c654ada15a4e0261a0db10cdcf692510be3ab2
Tomas Huang <huangguan3112789@gmail.com>
2016-11-09 16:25:33 +0800

    Bowling game.

---
+++ src/bowling.js
@@ -0,0 +1,176 @@
+var _ = require("lodash")
+
+class BowlingSM {
+  constructor(){
+    this.currentFrameIndex = 0
+    this.frames = [0,0,0,0,0,0,0,0,0,0]
+    this.action = this.firstThrow
+  }
+
+  get score(){
+    let count = 0
+    for(let i = 0;i<10;i++){
+      count += this.frames[i]
+    }
+    return count
+  }
+
+  bThrow(throws){
+    for(let i = 0;i<throws.length;i++){
+      // console.log(this.currentFrameIndex)
+      // console.log(i)
+      this.throw(throws[i])
+    }
+  }
+
+  throw(t){
+    // console.log(this.action)
+    this.action = this.action(t)
+  }
+
+  firstThrow(t){
+    if(t === 10){
+      this.frames[this.currentFrameIndex] = t
+      this.currentFrameIndex++
+      return this.afterStrike
+    }
+    return this.secondThrow(t)
+  }
+  secondThrow(t1){
+    return (t2) => {
+      this.frames[this.currentFrameIndex] = t1 + t2
+      this.currentFrameIndex++
+
+      if((t1+t2) === 10) {
+        return this.afterSpare
+      }
+
+      return this.firstThrow
+    }
+  }
+
+  afterSpare(t){
+    this.frames[this.currentFrameIndex-1] += t
+
+    if(t === 10){
+      return this.afterStrike
+    }
+
+    return this.secondThrow(t)
+  }
+  afterStrike(t){
+    this.frames[this.currentFrameIndex-1] += t    
+
+    if(t === 10){
+      this.frames[this.currentFrameIndex] = t
+      this.currentFrameIndex++ 
+
+      return this.afterStrikeStrike
+    }
+    return this.afterStrike2(t)
+
+  }
+
+  afterStrike2(t1){
+    return (t2) => {
+      this.frames[this.currentFrameIndex-1] += t2
+
+      this.frames[this.currentFrameIndex] = t1 + t2
+      this.currentFrameIndex++
+
+      return (t1+t2) === 10 ? this.afterSpare : this.firstThrow
+    }
+  }
+  afterStrikeStrike(t){
+    this.frames[this.currentFrameIndex - 2] += t
+    this.frames[this.currentFrameIndex - 1] += t
+
+    if(t === 10){
+      this.frames[this.currentFrameIndex] = t
+      this.currentFrameIndex++
+      return this.afterStrikeStrike
+    }
+
+    return this.afterStrike2(t)
+  }
+}
+
+class Frame {
+  constructor(){
+    this.throws = []
+    this.pass = false
+  }
+  get score(){
+    return _.sum(this.throws)
+  }
+  get isFull(){
+    if(this.throws[0] === 10 && this.throws.length >= 3)
+      return true
+
+    if((this.throws[0] + this.throws[1]) === 10 && this.throws.length >= 3)
+      return true
+
+    if((this.throws[0] + this.throws[1]) < 10 && this.throws.length >= 2) 
+      return true
+
+    return false
+  }
+  throw(t){
+    if(this.isFull) return
+
+    this.throws.push(t)
+
+    if(t === 10){
+      this.pass = true
+    }
+    if(this.throws.length === 2){
+      this.pass = true
+    }
+  }
+}
+
+class Bowling {
+  constructor(){
+    this.currentFrameIndex = 0
+    this.frames = [new Frame()]
+  }
+
+  get score(){
+    let count = 0
+    for(let i = 0;i<10;i++){
+      count += this.frames[i].score
+    }
+    return count
+  }
+  get currentFrame(){
+    return this.frames[this.currentFrameIndex]
+  }
+
+  bThrow(throws){
+    for(let i = 0;i<throws.length;i++){
+      console.log(i, throws[i])
+      console.log(this.frames)
+      this.throw(throws[i])
+    }
+  }
+
+  throw(t){
+    if(this.currentFrameIndex>=2) this.frames[this.currentFrameIndex-2].throw(t)
+
+    if(this.currentFrameIndex>=1) this.frames[this.currentFrameIndex-1].throw(t) 
+
+    this.currentFrame.throw(t)
+    if(this.currentFrame.pass){
+      this.currentFrameIndex++
+      this.frames.push(new Frame())
+    }
+
+    // console.log(this.action)
+    
+  }
+}
+
+// module.exports = BowlingSM
+module.exports = Bowling
+
+
---
+++ test/bowling.test.js
@@ -0,0 +1,42 @@
+require("mocha")
+var expect = require("chai").expect
+var _ = require("lodash")
+
+describe.only("Bowling", function(){
+  var Bowling = require("../src/bowling")
+  it("Normal", function(){
+    const bowling = new Bowling()
+    bowling.bThrow([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2])
+    expect(bowling.score).to.eql(40)
+  })
+
+  it("Normal", function(){
+    const bowling = new Bowling()
+    bowling.bThrow([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,2,0,2])
+    expect(bowling.score).to.eql(36)
+  })
+
+  it("Spare", function(){
+    const bowling = new Bowling()
+    bowling.bThrow([2,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2])
+    expect(bowling.score).to.eql(48)
+  })
+
+  it("Spare", function(){
+    const bowling = new Bowling()
+    bowling.bThrow([2,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,8,10])
+    expect(bowling.score).to.eql(64)
+  })
+
+  it("Strike1", function(){
+    const bowling = new Bowling()
+    bowling.bThrow([10,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2])
+    expect(bowling.score).to.eql(50)
+  })
+
+  it("Strike2", function(){
+    const bowling = new Bowling()
+    bowling.bThrow([10,10,10,10,10,10,10,10,10,10,10,10])
+    expect(bowling.score).to.eql(300)
+  })
+})
