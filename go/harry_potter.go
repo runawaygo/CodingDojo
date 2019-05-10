ff1f9d832858c9de26cb032d8630280092e39467
huangguan.518 <huangguan.518@bytedance.com>
2019-05-10 13:04:30 +0800

    Five book.

--- go/harry_potter.go
+++ go/harry_potter.go
@@ -1,5 +1,9 @@
 package harry_potter
 
-func Answer(n ...int32) float64 {
-	return float64(8 * n[0])
+func Answer(n ...float64) float64 {
+
+	n = append([]float64{0, 0, 0, 0, 0}, n...)
+	n = n[len(n)-5:]
+
+	return 8*5*n[0]*0.75 + 8*4*(n[1]-n[0])*0.8 + 8*3*(n[2]-n[1])*0.9 + 8*2*(n[3]-n[2])*0.95 + 8*1*(n[4]-n[3])*1
 }
--- go/harry_potter_test.go
+++ go/harry_potter_test.go
@@ -6,38 +6,92 @@ import (
 )
 
 func TestAnswer(t *testing.T) {
-	t.Run("Should pay 8 Dollar when buy 1 Book", func(t *testing.T) {
-		result := Answer(1)
-		if result != 8 {
-			t.Errorf("Expected:%d, Actual:%f", 8, result)
-		}
+	t.Run("1 kind of books", func(t *testing.T) {
+		t.Run("Should pay 8 Dollar when buy 1 Book", func(t *testing.T) {
+			actual := Answer(1)
+			assert(8.0, actual, t)
+		})
+
+		t.Run("Should pay 16 Dollar when buy 2 Book", func(t *testing.T) {
+			actual := Answer(2)
+			assert(16.0, actual, t)
+		})
+
+		t.Run("Should pay 8*n Dollar when buy n Book", func(t *testing.T) {
+			n := float64(rand.Int31() >> 3)
+
+			actual := Answer(n)
+			expect := float64(8 * n)
+
+			assert(actual, expect, t)
+		})
 	})
 
-	t.Run("Should pay 16 Dollar when buy 2 Book", func(t *testing.T) {
-		result := Answer(2)
-		if result != 16 {
-			t.Errorf("Expected:%d, Actual:%f", 16, result)
-		}
+	t.Run("2 kinds of books", func(t *testing.T) {
+		t.Run("Should pay 15.2 Dollar when buy [1, 1] Book", func(t *testing.T) {
+
+			actual := Answer(1, 1)
+			expect := float64((8 + 8) * 0.95)
+			assert(actual, expect, t)
+		})
+
+		t.Run("Should pay 23.2 Dollar when buy [1,2] Book", func(t *testing.T) {
+
+			actual := Answer(1, 2)
+			expect := float64((8+8)*0.95 + 8)
+			assert(actual, expect, t)
+		})
 
 	})
 
-	t.Run("Should pay 8*n Dollar when buy n Book", func(t *testing.T) {
-		n := rand.Int31() >> 3
+	t.Run("3 kinds of books", func(t *testing.T) {
+		t.Run("Should pay 21.6 Dollar when buy [1, 1, 1] Book", func(t *testing.T) {
 
-		result := Answer(n)
-		expect := float64(8 * n)
-		if result != expect {
-			t.Errorf("Expected:%f, Actual:%f", expect, result)
-		}
+			actual := Answer(1, 1, 1)
+			expect := 8 * 3 * 0.9
+			assert(actual, expect, t)
+		})
+
+		t.Run("Should pay 23.2 + 21.6 Dollar when buy [1,2,3] Book", func(t *testing.T) {
+
+			actual := Answer(1, 2, 3)
+			expect := float64(23.2 + 21.6)
+			assert(actual, expect, t)
+		})
+
+	})
+
+	t.Run("4 or 5 kinds of books", func(t *testing.T) {
+		t.Run("Should pay 25.6 Dollar when buy [1, 1, 1, 1] Book", func(t *testing.T) {
+
+			actual := Answer(1, 1, 1, 1)
+			expect := 8 * 4 * 0.8
+			assert(actual, expect, t)
+
+		})
+
+		t.Run("Tests set", func(t *testing.T) {
+
+			assert(Answer(1, 1, 1, 1, 1), 8*5*0.75, t)
+			assert(Answer(1, 2, 3, 4, 5), 100.4, t)
+		})
+
+		// t.Run("Should pay 23.2 + 21.6 Dollar when buy [1,2,3] Book", func(t *testing.T) {
+
+		// 	actual := Answer(1, 2, 3)
+		// 	expect := float64(23.2 + 21.6)
+		// 	assert(actual, expect, t)
+		// })
+
+	})
+
+	t.Run("Best discount", func(t *testing.T) {
+
 	})
 }
 
-func TestHandlerMultiBook(t *testing.T) {
-	t.Run("Should pay 8 Dollar when buy 1 Book", func(t *testing.T) {
-		result := Answer()
-		if result != 8 {
-			t.Errorf("Expected:%d, Actual:%f", 8, result)
-		}
-	})
-
+func assert(actual float64, expect float64, t *testing.T) {
+	if actual != expect {
+		t.Errorf("Expected:%f, Actual:%f", expect, actual)
+	}
 }
